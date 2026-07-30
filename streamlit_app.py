8539a689e81def1a542d2cfe792e9d528afc28d1
Aniketo18 <aniketpovhal@eaton.com>
2026-07-30 12:41:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,4 +57,5 @@ if ingredients_list:
 #new section to disply smoothiefroot nutrition information
  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response)
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
